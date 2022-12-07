e271da67e0aa91f220a888a402874ce7d4924705
Erika Pabon Triana <eriikapabon@gmail.com>
2022-12-07 15:22:45 -0800

    Working on Active controlls

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
-(function () {
-    [...document.querySelectorAll(".control")].forEach(button => {
-        button.addEventListener("click", function() {
-            document.querySelector(".active-btn").classList.remove("active-btn");
-            this.classList.add("active-btn");
-            document.querySelector(".active").classList.remove("active");
-            document.getElementById(button.dataset.id).classList.add("active");
+const sections = document.querySelectorAll('.section');//selecting all classes for buttons
+const sectBtns = document.querySelectorAll('.controlls');//parent
+const sectBtn = document.querySelectorAll('.control');//button
+const allselections= document.querySelector('.main-content');//selecting body/main class
+
+function PageTransitions(){
+    //buttons click active class
+    for (let i =0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function(){
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
         })
-    });
-    document.querySelector(".theme-btn").addEventListener("click", () => {
-        document.body.classList.toggle("light-mode");
+    }
+
+    //sections Active class
+    allselections.addEventListener('click', (e) =>{ //this function targets whatever dataset from HTML we toggle in
+        const id = e.target.dataset.id;//this put it in a variable called it id
+        if(id){
+            //remove selected from the other buttons
+            sectBtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //hide all other sections
+            sections.forEach((section)=>{
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);//whatever id is coming from id 
+            element.classList.add('active');
+        }
     })
-})();
+
+    //toggle theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', () =>{
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
+}
+
+PageTransitions();
